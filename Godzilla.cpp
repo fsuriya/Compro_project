1806ee6f57a445586f3cb1af14b686c99eededd1
Sirin0786 <35360503+Sirin0786@users.noreply.github.com>
2018-04-24 16:13:12 +0700

    Add files via upload

---
+++ Godzilla.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+int t=50;
+int main(){
+	
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "    " << setw(t) << "       ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "      \n";
+	cout << "   " << setw(t) << "      ";
+	//line
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,170);
+	cout << "--";
+	//ine
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "     \n";
+	cout << "  " << setw(t) << "     ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-------";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "  \n";
+	cout << " " << setw(t) << "    ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--------";
+	SetConsoleTextAttribute(hConsole,170);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << " \n";
+	cout << setw(t) << "   ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	//eye
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "---";
+	//eye
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "----";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "  \n";
+	cout << setw(t) << "   ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "---";
+	//mouth
+	SetConsoleTextAttribute(hConsole,34);
+	cout << ".";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "------";
+	SetConsoleTextAttribute(hConsole,170);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "\n";
+	cout << " " << setw(t) << "    ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "----------";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << " \n";
+	cout << "  " << setw(t) << "     ";
+	//arm
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,170);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "\n";
+	cout << "  " << setw(t) << "     ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,238);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,170);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "----";
+	SetConsoleTextAttribute(hConsole,170);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "-";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << " \n";
+	cout << "   " << setw(t) << "      ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--------";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "  \n";
+	cout << "   " << setw(t) << "      ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "  ";
+	SetConsoleTextAttribute(hConsole,34);
+	cout << "--";
+	SetConsoleTextAttribute(hConsole,0);
+	cout << "   \n";
+	
+	return 0;
+}
